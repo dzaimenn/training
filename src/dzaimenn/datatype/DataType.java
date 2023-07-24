8b94e4abdc15f900f888fff3409d5991546eca7b
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-07-24 17:25:02 +0300

    DataType v3

--- src/dzaimenn/datatype/DataType.java
+++ src/dzaimenn/datatype/DataType.java
@@ -38,5 +38,20 @@ public class DataType {
 
         boolean isJavaFun = true;
         System.out.println("Is Java Fun? " + isJavaFun);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
