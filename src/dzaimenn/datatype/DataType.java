a5b8c20f68174fdeac51ca4359e074e3b767c23a
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-07-24 20:40:52 +0300

    DataType v5

--- src/dzaimenn/datatype/DataType.java
+++ src/dzaimenn/datatype/DataType.java
@@ -4,41 +4,28 @@ import java.util.Scanner;
 
 public class DataType {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 5;
-        int sum = a + b;
-        int difference = a - b;
-        int product = a * b;
-        double division = (double) a / b;
-        int modulus = a % b;
+        int a = 10, b = 5;
+        double pi = 3.14159, radius = 2.5;
+        long bigNumber = 1234567890123456789L;
+        byte smallNumber = 127;
+        short mediumNumber = 32767;
+        char letter = 'A';
+        boolean isJavaFun = true;
+
+        int sum = a + b, difference = a - b, product = a * b, modulus = a % b;
+        double division = (double) a / b, area = pi * radius * radius, circumference = 2 * pi * radius;
 
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
         System.out.println("Division: " + division);
         System.out.println("Modulus: " + modulus);
-
-        double pi = 3.14159;
-        float radius = 2.5f;
-        double area = pi * radius * radius;
-        double circumference = 2 * pi * radius;
-
         System.out.println("Area of Circle: " + area);
         System.out.println("Circumference of Circle: " + circumference);
-
-        long bigNumber = 1234567890123456789L;
         System.out.println("Big Number: " + bigNumber);
-
-        byte smallNumber = 127;
         System.out.println("Small Number: " + smallNumber);
-
-        short mediumNumber = 32767;
         System.out.println("Medium Number: " + mediumNumber);
-
-        char letter = 'A';
         System.out.println("Letter: " + letter);
-
-        boolean isJavaFun = true;
         System.out.println("Is Java Fun? " + isJavaFun);
 
         for (int i = 1; i <= 10; i++) {
