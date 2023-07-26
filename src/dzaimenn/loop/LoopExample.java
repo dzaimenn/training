bc2e4312e49a8e598da00965db264c5bd49956fa
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-07-26 17:44:16 +0300

    Loops v3

--- src/dzaimenn/loop/LoopExample.java
+++ src/dzaimenn/loop/LoopExample.java
@@ -1,53 +1,64 @@
 package dzaimenn.loop;
 
+import java.util.Scanner;
+
 public class LoopExample {
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                matrix[i][j] = i * 10 + j;
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Multiplication table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+        int[] numbers = {3, 6, 9, 12, 15};
+        System.out.println("Numbers divisible by 3:");
+        for (int num : numbers) {
+            if (num % 3 == 0) {
+                System.out.println(num);
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += i;
-        }
-        System.out.println("Sum of numbers from 0 to 9: " + sum);
-
-        int[] fibonacci = new int[20];
-        fibonacci[0] = 0;
-        fibonacci[1] = 1;
-
-        for (int i = 2; i < 20; i++) {
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-        }
-
-        System.out.print("First 20 Fibonacci numbers: ");
-        for (int i = 0; i < 20; i++) {
-            System.out.print(fibonacci[i] + " ");
+        System.out.println("Countdown from 10 to 1:");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
-        String[] names = {"Alice", "Bob", "Charlie", "David", "Emma"};
-
-        for (String name : names) {
-            System.out.println("Hello, " + name + "!");
+        int total = 0;
+        for (int i = 0; i <= 100; i += 5) {
+            total += i;
         }
+        System.out.println("Sum of numbers from 0 to 100 (increment by 5): " + total);
 
-        int factorial = 1;
-        for (int i = 1; i <= 5; i++) {
-            factorial *= i;
+        int[] array = {2, 4, 6, 8, 10};
+        int product = 1;
+        for (int num : array) {
+            product *= num;
         }
-        System.out.println("Factorial of 5: " + factorial);
+        System.out.println("Product of numbers in the array: " + product);
+
+        System.out.println("Even numbers from 1 to 20:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("Odd numbers from 1 to 20:");
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("Square numbers from 1 to 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i * i + " ");
+        }
+        System.out.println();
+
+        scanner.close();
     }
-    
 }
