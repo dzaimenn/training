6fae033a169c582e4c9727e68e9e317ed41d4dd4
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-07-27 19:52:14 +0300

    Loops v4

--- src/dzaimenn/loop/LoopExample.java
+++ src/dzaimenn/loop/LoopExample.java
@@ -3,20 +3,21 @@ package dzaimenn.loop;
 import java.util.Scanner;
 
 public class LoopExample {
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.print("Please input a number: ");
         int number = scanner.nextInt();
 
-        System.out.println("Multiplication table for " + number + ":");
+        System.out.println("Multiplication table for the number " + number + ":");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " x " + i + " = " + (number * i));
+            System.out.println(number + " multiplied by " + i + " equals " + (number * i));
         }
 
-        int[] numbers = {3, 6, 9, 12, 15};
+        int[] divisibleByThree = {3, 6, 9, 12, 15};
         System.out.println("Numbers divisible by 3:");
-        for (int num : numbers) {
+        for (int num : divisibleByThree) {
             if (num % 3 == 0) {
                 System.out.println(num);
             }
@@ -28,11 +29,11 @@ public class LoopExample {
         }
         System.out.println();
 
-        int total = 0;
+        int sum = 0;
         for (int i = 0; i <= 100; i += 5) {
-            total += i;
+            sum += i;
         }
-        System.out.println("Sum of numbers from 0 to 100 (increment by 5): " + total);
+        System.out.println("Sum of numbers from 0 to 100 (increasing by 5): " + sum);
 
         int[] array = {2, 4, 6, 8, 10};
         int product = 1;
@@ -55,7 +56,7 @@ public class LoopExample {
 
         System.out.println("Square numbers from 1 to 10:");
         for (int i = 1; i <= 10; i++) {
-            System.out.print(i * i + " ");
+            System.out.print("Square of " + i + " is " + (i * i) + ", ");
         }
         System.out.println();
 
