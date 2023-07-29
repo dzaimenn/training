130162c6b510d93047e77c35e00b24af5ce5c0a8
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-07-29 21:07:56 +0300

    Loops new class v2

--- src/dzaimenn/loop/LoopExample2.java
+++ src/dzaimenn/loop/LoopExample2.java
@@ -6,6 +6,8 @@ public class LoopExample2 {
 
         String message = "JAVA ";
         int numLoops = 5;
+        int maxNumber = 30;
+        boolean printStars = true;
 
         for (int i = 0; i < numLoops; i++) {
             System.out.print(message);
@@ -21,7 +23,11 @@ public class LoopExample2 {
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                if (printStars) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(i * j + " ");
+                }
             }
             System.out.println();
         }
@@ -30,16 +36,19 @@ public class LoopExample2 {
 
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                if (printStars) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(i * j + " ");
+                }
             }
             System.out.println();
         }
 
         System.out.println();
 
-        for (int i = 2; i <= 20; i += 2) {
+        for (int i = 2; i <= maxNumber; i += 2) {
             System.out.print(i + " ");
         }
     }
-
 }
