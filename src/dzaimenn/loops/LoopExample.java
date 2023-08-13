c9e6266157143e7eadbf741170d27a552e7dd214
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-08-13 19:27:33 +0300

    Loops

--- src/dzaimenn/loops/LoopExample.java
+++ src/dzaimenn/loops/LoopExample.java
@@ -2,4 +2,70 @@ package dzaimenn.loops;
 
 public class LoopExample {
 
+    public static void main(String[] args) {
+        
+        int rows = 5;
+        int cols = 10;
+
+        System.out.println("Printing a simple pattern using loops:");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPrinting numbers in a triangle pattern:");
+        int num = 1;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPrinting a simple multiplication table:");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPrinting numbers in a reverse triangle pattern:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPrinting a simple right triangle pattern:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nPrinting numbers in a pyramid pattern:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print(l + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
