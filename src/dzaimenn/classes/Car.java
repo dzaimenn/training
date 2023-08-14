6259a9092920913316a289a4dbd60a36be2a7f99
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-08-14 19:34:54 +0300

    Classes added data

--- src/dzaimenn/classes/Car.java
+++ src/dzaimenn/classes/Car.java
@@ -1,4 +1,20 @@
 package dzaimenn.classes;
 
 public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
 }
--- src/dzaimenn/classes/Main.java
+++ src/dzaimenn/classes/Main.java
@@ -2,6 +2,11 @@ package dzaimenn.classes;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Camry", 2020);
+        Car car2 = new Car("Honda", "Civic", 2018);
 
+        car1.displayInfo();
+        car2.displayInfo();
     }
+    
 }
