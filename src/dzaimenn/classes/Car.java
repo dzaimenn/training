37fee528adfde41f230c5fb7a4538e02df10b301
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-08-19 18:51:09 +0300

    Collections added

--- src/dzaimenn/classes/Car.java
+++ src/dzaimenn/classes/Car.java
@@ -42,5 +42,5 @@ public class Car {
     public void addFeature(String feature) {
         features.add(feature);
     }
-    
+
 }
---
+++ src/dzaimenn/collections/Main.java
@@ -0,0 +1,11 @@
+package dzaimenn.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
