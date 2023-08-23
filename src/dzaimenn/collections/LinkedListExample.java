0d19a68da6618ccc583b89a0a1ec1d0de2bc9f9a
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-08-23 21:57:13 +0300

    Collections LinkedList v2

--- src/dzaimenn/collections/LinkedListExample.java
+++ src/dzaimenn/collections/LinkedListExample.java
@@ -9,43 +9,52 @@ public class LinkedListExample {
 
         LinkedList<String> linkedList = new LinkedList<>();
         Vector<String> vector = new Vector<>();
-        linkedList.add("Element 1");
-        linkedList.add("Element 2");
-        linkedList.add("Element 3");
-        linkedList.add("Element 4");
-        linkedList.add("Element 5");
 
-        vector.add("Item 1");
-        vector.add("Item 2");
-        vector.add("Item 3");
-        vector.add("Item 4");
-        vector.add("Item 5");
-        System.out.println("LinkedList Elements:");
+        linkedList.add("Apple");
+        linkedList.add("Banana");
+        linkedList.add("Cherry");
+        linkedList.add("Date");
+        linkedList.add("Fig");
 
-        for (String element : linkedList) {
-            System.out.println(element);
+        vector.add("Grapes");
+        vector.add("Honeydew");
+        vector.add("Kiwi");
+        vector.add("Lemon");
+        vector.add("Mango");
+
+        System.out.println("Fruits in the LinkedList:");
+        for (String fruit : linkedList) {
+            System.out.println(fruit);
         }
-        System.out.println("Vector Elements:");
 
-        for (String item : vector) {
-            System.out.println(item);
+        System.out.println("Fruits in the Vector:");
+        for (String fruit : vector) {
+            System.out.println(fruit);
         }
-        linkedList.remove("Element 3");
+
+        linkedList.remove("Cherry");
         linkedList.removeFirst();
         linkedList.removeLast();
-        vector.remove("Item 3");
+
+        vector.remove("Kiwi");
         vector.removeElementAt(0);
         vector.removeElementAt(vector.size() - 1);
+
         System.out.println("Modified LinkedList:");
-
-        for (String element : linkedList) {
-            System.out.println(element);
+        for (String fruit : linkedList) {
+            System.out.println(fruit);
         }
-        
+
         System.out.println("Modified Vector:");
-        for (String item : vector) {
-            System.out.println(item);
+        for (String fruit : vector) {
+            System.out.println(fruit);
         }
-    }
 
+        System.out.println("Size of the LinkedList: " + linkedList.size());
+        System.out.println("Size of the Vector: " + vector.size());
+
+        System.out.println("Does LinkedList contain 'Banana'? " + linkedList.contains("Banana"));
+        System.out.println("Does Vector contain 'Lemon'? " + vector.contains("Lemon"));
+    }
+    
 }
