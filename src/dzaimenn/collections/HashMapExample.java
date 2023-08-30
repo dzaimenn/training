c97f7ff96fa5a22f0409a0d00ce75b455309b918
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-08-30 20:19:59 +0300

    Collections HashMap v2

--- src/dzaimenn/collections/HashMapExample.java
+++ src/dzaimenn/collections/HashMapExample.java
@@ -1,7 +1,36 @@
 package dzaimenn.collections;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class HashMapExample {
+
     public static void main(String[] args) {
-        
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "Apple");
+        hashMap.put(2, "Banana");
+        hashMap.put(3, "Cherry");
+        hashMap.put(4, "Date");
+        hashMap.put(5, "Elderberry");
+        System.out.println("HashMap:");
+
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put(1, "Ant");
+        linkedHashMap.put(2, "Bear");
+        linkedHashMap.put(3, "Cat");
+        linkedHashMap.put(4, "Dog");
+        linkedHashMap.put(5, "Elephant");
+        System.out.println("\nLinkedHashMap:");
+
+        for (Map.Entry<Integer, String> entry : linkedHashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
     }
+    
 }
