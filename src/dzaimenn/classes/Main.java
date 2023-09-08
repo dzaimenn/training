8cee3514748eae4ca650a41aff633207e0641624
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-08 20:33:54 +0300

    Car collections

--- src/dzaimenn/classes/Main.java
+++ src/dzaimenn/classes/Main.java
@@ -1,8 +1,30 @@
 package dzaimenn.classes;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("Toyota", "Camry", 2020));
+        cars.add(new Car("Honda", "Civic", 2018));
+        cars.add(new Car("Ford", "Fusion", 2019));
+        cars.add(new Car("Tesla", "Model S", 2022));
+
+        System.out.println("List of cars:");
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+
+        System.out.println("Removing the car at index 2:");
+        cars.remove(2);
+
+        System.out.println("Updated list of cars:");
+        for (Car car : cars) {
+            System.out.println(car);
+        }
     }
 
 }
