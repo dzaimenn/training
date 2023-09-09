3959e7e5fdd9927272a4ed0dd87a04005f97d49c
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-09 20:27:43 +0300

    Car collections v4

--- src/dzaimenn/classes/Main.java
+++ src/dzaimenn/classes/Main.java
@@ -17,36 +17,30 @@ public class Main {
         cars.add(new Car("Tesla", "Model S", 2022));
 
         System.out.println("List of cars:");
-        for (Car car : cars) {
-            System.out.println(car);
-        }
+        cars.forEach(System.out::println);
 
+        System.out.println("Populating the car map...");
         for (Car car : cars) {
             carMap.put(car.getBrand(), car);
         }
 
         System.out.println("Map of cars:");
-        for (Map.Entry<String, Car> entry : carMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        carMap.forEach((brand, car) -> System.out.println(brand + ": " + car));
 
         List<Car> oldCars = new ArrayList<>();
+        System.out.println("Identifying old cars (older than 5 years)...");
         for (Car car : cars) {
             if (2024 - car.getYear() > 5) {
                 oldCars.add(car);
             }
         }
 
-        System.out.println("Old cars (older than 5 years):");
-        for (Car car : oldCars) {
-            System.out.println(car);
-            cars.remove(car);
-        }
+        System.out.println("Old cars:");
+        oldCars.forEach(System.out::println);
+        cars.removeAll(oldCars);
 
         System.out.println("Updated list of cars:");
-        for (Car car : cars) {
-            System.out.println(car);
-        }
+        cars.forEach(System.out::println);
     }
-    
+
 }
