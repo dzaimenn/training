cbfd7d6a567dd58643cc63fa01fc95d1eaa114b2
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-09 19:07:23 +0300

    Car collections v2

--- src/dzaimenn/classes/Main.java
+++ src/dzaimenn/classes/Main.java
@@ -1,12 +1,15 @@
 package dzaimenn.classes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
+        Map<String, Car> carMap = new HashMap<>();
 
         cars.add(new Car("Toyota", "Camry", 2020));
         cars.add(new Car("Honda", "Civic", 2018));
@@ -18,12 +21,14 @@ public class Main {
             System.out.println(car);
         }
 
-        System.out.println("Removing the car at index 2:");
-        cars.remove(2);
+        carMap.put("Toyota", new Car("Toyota", "Camry", 2020));
+        carMap.put("Honda", new Car("Honda", "Civic", 2018));
+        carMap.put("Ford", new Car("Ford", "Fusion", 2019));
+        carMap.put("Tesla", new Car("Tesla", "Model S", 2022));
 
-        System.out.println("Updated list of cars:");
-        for (Car car : cars) {
-            System.out.println(car);
+        System.out.println("Map of cars:");
+        for (Map.Entry<String, Car> entry : carMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
