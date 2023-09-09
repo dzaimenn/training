0e773cc7e673f9e6acb3b61a49b61b07e4b5b0cd
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-09 19:55:08 +0300

    Car collections v3

--- src/dzaimenn/classes/Main.java
+++ src/dzaimenn/classes/Main.java
@@ -21,15 +21,32 @@ public class Main {
             System.out.println(car);
         }
 
-        carMap.put("Toyota", new Car("Toyota", "Camry", 2020));
-        carMap.put("Honda", new Car("Honda", "Civic", 2018));
-        carMap.put("Ford", new Car("Ford", "Fusion", 2019));
-        carMap.put("Tesla", new Car("Tesla", "Model S", 2022));
+        for (Car car : cars) {
+            carMap.put(car.getBrand(), car);
+        }
 
         System.out.println("Map of cars:");
         for (Map.Entry<String, Car> entry : carMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-    }
 
+        List<Car> oldCars = new ArrayList<>();
+        for (Car car : cars) {
+            if (2024 - car.getYear() > 5) {
+                oldCars.add(car);
+            }
+        }
+
+        System.out.println("Old cars (older than 5 years):");
+        for (Car car : oldCars) {
+            System.out.println(car);
+            cars.remove(car);
+        }
+
+        System.out.println("Updated list of cars:");
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+    
 }
