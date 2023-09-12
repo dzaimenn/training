924a921a2b63de5948eb1f879593937991cfb4dd
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-12 20:34:07 +0300

    IO Streams

---
+++ src/dzaimenn/streamsio/Main.java
@@ -0,0 +1,25 @@
+package dzaimenn.streamsio;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("example.txt");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+}
