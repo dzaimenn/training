0d5e13a4ee9c6339d3d9a471647b94ebb4a59b94
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-18 21:02:40 +0300

    Added StreamExample2 logic

--- src/dzaimenn/streamsio/StreamExample2.java
+++ src/dzaimenn/streamsio/StreamExample2.java
@@ -1,5 +1,36 @@
 package dzaimenn.streamsio;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
 public class StreamExample2 {
+    public static void main(String[] args) {
+        try {
+            Process process = Runtime.getRuntime().exec("ping www.example.com");
+            InputStream inputStream = process.getInputStream();
+            OutputStream outputStream = process.getOutputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                writer.println("Data from process: " + line);
+                writer.flush();
+            }
+
+            reader.close();
+            writer.close();
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
