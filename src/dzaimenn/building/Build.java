6933378f996a968a01ce09c3078466c51f0ac2a8
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-20 20:08:11 +0300

    Added Build and Citizen logic

--- src/dzaimenn/building/Build.java
+++ src/dzaimenn/building/Build.java
@@ -1,4 +1,38 @@
 package dzaimenn.building;
 
 public class Build {
+    private String type;
+    private int numberOfFloors;
+    private String address;
+
+    public Build(String type, int numberOfFloors, String address) {
+        this.type = type;
+        this.numberOfFloors = numberOfFloors;
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
