0d9a432928bf7dbba0fe671cc750471fe7aadb41
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-20 21:33:22 +0300

    Added Main logic

--- src/dzaimenn/building/Citizen.java
+++ src/dzaimenn/building/Citizen.java
@@ -34,5 +34,5 @@ public class Citizen {
     public void setAddress(String address) {
         this.address = address;
     }
-    
+
 }
--- src/dzaimenn/building/Main.java
+++ src/dzaimenn/building/Main.java
@@ -3,7 +3,33 @@ package dzaimenn.building;
 public class Main {
 
     public static void main(String[] args) {
+        Build building = new Build("Office", 10, "123 Main Street");
 
+        System.out.println("Building Type: " + building.getType());
+        System.out.println("Number of Floors: " + building.getNumberOfFloors());
+        System.out.println("Address: " + building.getAddress());
+
+        building.setType("Residential");
+        building.setNumberOfFloors(15);
+        building.setAddress("456 Elm Street");
+
+        System.out.println("\nUpdated Building Type: " + building.getType());
+        System.out.println("Updated Number of Floors: " + building.getNumberOfFloors());
+        System.out.println("Updated Address: " + building.getAddress());
+
+        Citizen citizen = new Citizen("John Doe", 30, "789 Oak Avenue");
+
+        System.out.println("\nCitizen Name: " + citizen.getName());
+        System.out.println("Citizen Age: " + citizen.getAge());
+        System.out.println("Citizen Address: " + citizen.getAddress());
+
+        citizen.setName("Jane Smith");
+        citizen.setAge(25);
+        citizen.setAddress("321 Pine Street");
+
+        System.out.println("\nUpdated Citizen Name: " + citizen.getName());
+        System.out.println("Updated Citizen Age: " + citizen.getAge());
+        System.out.println("Updated Citizen Address: " + citizen.getAddress());
     }
 
 }
