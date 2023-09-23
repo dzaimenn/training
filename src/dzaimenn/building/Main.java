a1a897a0fc21383735e978969025d3a4d3afe0a0
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-23 22:11:01 +0300

    refactoring all logic

--- src/dzaimenn/building/Main.java
+++ src/dzaimenn/building/Main.java
@@ -1,33 +1,99 @@
 package dzaimenn.building;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Build> buildings = new ArrayList<>();
-        buildings.add(new Build("Office", 10, "123 Main Street"));
-        buildings.add(new Build("Residential", 5, "456 Elm Street"));
-        buildings.add(new Build("School", 3, "789 Oak Avenue"));
+        City city = new City("Metropolis");
 
-        List<Citizen> citizens = new ArrayList<>();
-        citizens.add(new Citizen("John Doe", 30, "123 Main Street"));
-        citizens.add(new Citizen("Jane Smith", 25, "456 Elm Street"));
-        citizens.add(new Citizen("Bob Johnson", 40, "789 Oak Avenue"));
+        Map<String, District> districts = new HashMap<>();
+        districts.put("Downtown", new District("Downtown"));
+        districts.put("Residential Area", new District("Residential Area"));
 
-        City city = new City("Metropolis", buildings, citizens);
+        districts.get("Downtown").addBuilding(new Build("Office", 10, "1 Main Street"));
+        districts.get("Downtown").addBuilding(new Build("Shop", 3, "2 Elm Street"));
+        districts.get("Residential Area").addBuilding(new Build("Apartment", 5, "10 Oak Avenue"));
+        districts.get("Residential Area").addBuilding(new Build("House", 2, "20 Pine Street"));
+
+        districts.get("Downtown").addCitizen(new Citizen("John Doe", 30, "1 Main Street"));
+        districts.get("Downtown").addCitizen(new Citizen("Jane Smith", 25, "2 Elm Street"));
+        districts.get("Residential Area").addCitizen(new Citizen("Bob Johnson", 40, "10 Oak Avenue"));
+        districts.get("Residential Area").addCitizen(new Citizen("Alice Brown", 35, "20 Pine Street"));
+
+        for (District district : districts.values()) {
+            city.addDistrict(district);
+        }
 
         System.out.println("City: " + city.getName());
-        System.out.println("List of Buildings:");
-        for (Build building : city.getBuildings()) {
-            System.out.println("Type: " + building.getType() + ", Floors: " + building.getNumberOfFloors() + ", Address: " + building.getAddress());
+        for (District district : city.getDistricts()) {
+            System.out.println("\nDistrict: " + district.getName());
+            System.out.println("Buildings:");
+            for (Build building : district.getBuildings()) {
+                System.out.println("- Type: " + building.getType() + ", Floors: " + building.getNumberOfFloors() + ", Address: " + building.getAddress());
+            }
+            System.out.println("Citizens:");
+            for (Citizen citizen : district.getCitizens()) {
+                System.out.println("- Name: " + citizen.getName() + ", Age: " + citizen.getAge() + ", Address: " + citizen.getAddress());
+            }
         }
+    }
+}
 
-        System.out.println("\nList of Citizens:");
-        for (Citizen citizen : city.getCitizens()) {
-            System.out.println("Name: " + citizen.getName() + ", Age: " + citizen.getAge() + ", Address: " + citizen.getAddress());
-        }
+class City {
+    private String name;
+    private List<District> districts;
+
+    public City(String name) {
+        this.name = name;
+        this.districts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<District> getDistricts() {
+        return districts;
+    }
+
+    public void addDistrict(District district) {
+        districts.add(district);
+    }
+}
+
+class District {
+    private String name;
+    private List<Build> buildings;
+    private List<Citizen> citizens;
+
+    public District(String name) {
+        this.name = name;
+        this.buildings = new ArrayList<>();
+        this.citizens = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Build> getBuildings() {
+        return buildings;
+    }
+
+    public List<Citizen> getCitizens() {
+        return citizens;
+    }
+
+    public void addBuilding(Build building) {
+        buildings.add(building);
+    }
+
+    public void addCitizen(Citizen citizen) {
+        citizens.add(citizen);
     }
     
 }
