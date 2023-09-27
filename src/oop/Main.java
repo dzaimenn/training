d8576f51de3e8d19c5d175ef3e5de263642e50da
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-09-27 18:27:45 +0300

    OOP Main v2

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -1,28 +1,34 @@
 package oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
         Car myCar = new Car("Toyota", "Camry", 2022, 25000.0);
+        Car friendCar = new Car("Ford", "Focus", 2021, 20000.0);
 
-        myCar.displayCarInfo();
-        System.out.println();
+        List<Car> carList = new ArrayList<>();
+        carList.add(myCar);
+        carList.add(friendCar);
 
-        myCar.startEngine();
-        myCar.displayCarInfo();
-        System.out.println();
+        System.out.println("Cars in the list:");
+        for (Car car : carList) {
+            car.displayCarInfo();
+            System.out.println();
+        }
 
-        myCar.stopEngine();
-        myCar.displayCarInfo();
-        System.out.println();
+        System.out.println("Starting engines:");
+        for (Car car : carList) {
+            car.startEngine();
+        }
 
-        myCar.setPrice(26000.0);
-        myCar.displayCarInfo();
-        System.out.println();
-
-        myCar.setBrand("Honda");
-        myCar.setModel("Accord");
-        myCar.setYear(2023);
-        myCar.displayCarInfo();
+        System.out.println("Cars after starting engines:");
+        for (Car car : carList) {
+            car.displayCarInfo();
+            System.out.println();
+        }
     }
-    
+
 }
