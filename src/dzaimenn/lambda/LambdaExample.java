e2eb4c4b79d88f9c6e1e4fcb379d1791b4e7b734
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-03 18:14:28 +0300

    Lambda v2

--- src/dzaimenn/lambda/LambdaExample.java
+++ src/dzaimenn/lambda/LambdaExample.java
@@ -2,30 +2,33 @@ package dzaimenn.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class LambdaExample {
 
     public static void main(String[] args) {
-
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
             numbers.add(i);
         }
 
         System.out.println("Even numbers:");
-        numbers.stream().filter(isEven()).forEach(System.out::println);
+        numbers.stream().filter(isMultipleOf(2)).forEach(System.out::println);
 
         System.out.println("Odd numbers:");
-        numbers.stream().filter(isOdd()).forEach(System.out::println);
+        numbers.stream().filter(isMultipleOf(2).negate()).forEach(System.out::println);
+
+        System.out.println("Squares of numbers:");
+        numbers.stream().map(square()).forEach(System.out::println);
     }
 
-    private static Predicate<Integer> isEven() {
-        return n -> n % 2 == 0;
+    private static Predicate<Integer> isMultipleOf(int divisor) {
+        return n -> n % divisor == 0;
     }
 
-    private static Predicate<Integer> isOdd() {
-        return n -> n % 2 != 0;
+    private static Function<Integer, Integer> square() {
+        return n -> n * n;
     }
-
+    
 }
