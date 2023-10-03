bfb005b872f91ac82053b16654d5f9fbddaaf2ba
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-03 18:54:33 +0300

    Lambda v3

--- src/dzaimenn/lambda/LambdaExample.java
+++ src/dzaimenn/lambda/LambdaExample.java
@@ -8,27 +8,33 @@ import java.util.function.Predicate;
 public class LambdaExample {
 
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= 100; i++) {
-            numbers.add(i);
-        }
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("orange");
+        words.add("grape");
+        words.add("watermelon");
 
-        System.out.println("Even numbers:");
-        numbers.stream().filter(isMultipleOf(2)).forEach(System.out::println);
+        System.out.println("Words starting with 'a':");
+        words.stream().filter(startsWith("a")).forEach(System.out::println);
 
-        System.out.println("Odd numbers:");
-        numbers.stream().filter(isMultipleOf(2).negate()).forEach(System.out::println);
+        System.out.println("Words ending with 'e':");
+        words.stream().filter(endsWith("e")).forEach(System.out::println);
 
-        System.out.println("Squares of numbers:");
-        numbers.stream().map(square()).forEach(System.out::println);
+        System.out.println("Uppercase words:");
+        words.stream().map(toUpperCase()).forEach(System.out::println);
     }
 
-    private static Predicate<Integer> isMultipleOf(int divisor) {
-        return n -> n % divisor == 0;
+    private static Predicate<String> startsWith(String prefix) {
+        return s -> s.startsWith(prefix);
     }
 
-    private static Function<Integer, Integer> square() {
-        return n -> n * n;
+    private static Predicate<String> endsWith(String suffix) {
+        return s -> s.endsWith(suffix);
+    }
+
+    private static Function<String, String> toUpperCase() {
+        return String::toUpperCase;
     }
     
 }
