ac27d827ddb41c31970ba7bc45a0fa4c659e0e3c
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-11 19:17:36 +0300

    Added logic MethodOverloadingExample

--- src/dzaimenn/overloading/MethodOverloadingExample.java
+++ src/dzaimenn/overloading/MethodOverloadingExample.java
@@ -1,9 +1,8 @@
 package dzaimenn.overloading;
 
 public class MethodOverloadingExample {
-    public static void main(String[] args) {
 
-    }
+    private String message;
 
     public MethodOverloadingExample() {
         this("Default Message");
@@ -12,4 +11,37 @@ public class MethodOverloadingExample {
     public MethodOverloadingExample(String message) {
         this.message = message;
     }
+
+    public void displayMessage() {
+        System.out.println(message);
+    }
+
+    public void processNumber(int num) {
+        System.out.println("Processing integer: " + num);
+    }
+
+    public void processNumber(double num) {
+        System.out.println("Processing double: " + num);
+    }
+
+    public void processText(String text) {
+        System.out.println("Processing text: " + text);
+    }
+
+    public void processText(String text, int times) {
+        System.out.println("Processing text multiple times: " + text.repeat(times));
+    }
+
+    public static void main(String[] args) {
+        MethodOverloadingExample example1 = new MethodOverloadingExample();
+        example1.displayMessage();
+        example1.processNumber(42);
+        example1.processText("Java is awesome!");
+
+        MethodOverloadingExample example2 = new MethodOverloadingExample("Custom Message");
+        example2.displayMessage();
+        example2.processNumber(3.14);
+        example2.processText("Hello", 3);
+    }
+    
 }
