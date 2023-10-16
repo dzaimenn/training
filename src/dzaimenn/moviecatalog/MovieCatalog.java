3ec2e2c009a3c3d4072b9d30fabe19696981c0e2
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-16 18:20:13 +0300

    MovieCatalog v2

--- src/dzaimenn/moviecatalog/MovieCatalog.java
+++ src/dzaimenn/moviecatalog/MovieCatalog.java
@@ -1,18 +1,17 @@
 package dzaimenn.moviecatalog;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MovieCatalog {
-
     public static void main(String[] args) {
         System.out.println("Welcome to the Movie Catalog!");
 
-        List<String> movies = initializeMovies();
+        List<String> movies = Arrays.asList("The Shawshank Redemption", "The Godfather", "The Dark Knight", "Inception");
         displayMovies(movies);
 
         String searchMovie = "Inception";
-        boolean found = searchMovie(movies, searchMovie);
+        boolean found = movies.stream().anyMatch(movie -> movie.equalsIgnoreCase(searchMovie));
         if (found) {
             System.out.println("The movie '" + searchMovie + "' is in the catalog.");
         } else {
@@ -20,29 +19,9 @@ public class MovieCatalog {
         }
     }
 
-    private static List<String> initializeMovies() {
-        List<String> movies = new ArrayList<>();
-        movies.add("The Shawshank Redemption");
-        movies.add("The Godfather");
-        movies.add("The Dark Knight");
-        movies.add("Inception");
-        return movies;
-    }
-
     private static void displayMovies(List<String> movies) {
         System.out.println("Movies in the catalog:");
-        for (String movie : movies) {
-            System.out.println(movie);
-        }
+        movies.forEach(System.out::println);
     }
-
-    private static boolean searchMovie(List<String> movies, String searchMovie) {
-        for (String movie : movies) {
-            if (movie.equalsIgnoreCase(searchMovie)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+    
 }
