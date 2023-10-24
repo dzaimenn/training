ccd2cf8061f261fa759c0d3729759eda9128ae80
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-24 18:24:30 +0300

    after performing a calculation, the program prompts the user if they want to perform another calculation

--- src/dzaimenn/calculator/Calculator.java
+++ src/dzaimenn/calculator/Calculator.java
@@ -6,49 +6,56 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1, num2, result;
-        char operator;
+        char choice;
 
         System.out.println("Welcome to the Calculator!");
 
-        try {
-            System.out.print("Enter first number: ");
-            num1 = Double.parseDouble(scanner.nextLine());
+        do {
+            double num1, num2, result;
+            char operator;
 
-            System.out.print("Enter second number: ");
-            num2 = Double.parseDouble(scanner.nextLine());
-        } catch (NumberFormatException ex) {
-            System.out.println("Error! Invalid input. Please enter numeric values.");
-            return;
-        }
+            try {
+                System.out.print("Enter first number: ");
+                num1 = Double.parseDouble(scanner.nextLine());
 
-        System.out.print("Enter operator (+, -, *, /): ");
-        operator = scanner.nextLine().charAt(0);
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error! Division by zero is not allowed.");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Error! Invalid operator.");
+                System.out.print("Enter second number: ");
+                num2 = Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException ex) {
+                System.out.println("Error! Invalid input. Please enter numeric values.");
                 return;
-        }
+            }
 
-        System.out.println("Result: " + result);
+            System.out.print("Enter operator (+, -, *, /): ");
+            operator = scanner.nextLine().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error! Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Error! Invalid operator.");
+                    return;
+            }
+
+            System.out.println("Result: " + result);
+
+            System.out.print("Do you want to perform another calculation? (Y/N): ");
+            choice = scanner.nextLine().toUpperCase().charAt(0);
+        } while (choice == 'Y');
     }
     
 }
