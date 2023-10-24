2745d29fb9158a7d0996c7dd1ca4f365610efcb6
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-10-24 21:01:25 +0300

    Refactoring

--- src/dzaimenn/calculator/Calculator.java
+++ src/dzaimenn/calculator/Calculator.java
@@ -3,59 +3,52 @@ package dzaimenn.calculator;
 import java.util.Scanner;
 
 public class Calculator {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char choice;
+        double num1, num2, result;
+        char operator;
 
         System.out.println("Welcome to the Calculator!");
 
-        do {
-            double num1, num2, result;
-            char operator;
+        try {
+            System.out.print("Enter first number: ");
+            num1 = Double.parseDouble(scanner.nextLine());
 
-            try {
-                System.out.print("Enter first number: ");
-                num1 = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter second number: ");
+            num2 = Double.parseDouble(scanner.nextLine());
+        } catch (NumberFormatException ex) {
+            System.out.println("Error! Invalid input. Please enter numeric values.");
+            return;
+        }
 
-                System.out.print("Enter second number: ");
-                num2 = Double.parseDouble(scanner.nextLine());
-            } catch (NumberFormatException ex) {
-                System.out.println("Error! Invalid input. Please enter numeric values.");
-                return;
-            }
+        System.out.print("Enter operator (+, -, *, /): ");
+        operator = scanner.nextLine().charAt(0);
 
-            System.out.print("Enter operator (+, -, *, /): ");
-            operator = scanner.nextLine().charAt(0);
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("Error! Division by zero is not allowed.");
-                        return;
-                    }
-                    break;
-                default:
-                    System.out.println("Error! Invalid operator.");
-                    return;
-            }
-
+        result = calculate(num1, num2, operator);
+        if (Double.isNaN(result)) {
+            System.out.println("Error! Invalid operation or division by zero.");
+        } else {
             System.out.println("Result: " + result);
+        }
+    }
 
-            System.out.print("Do you want to perform another calculation? (Y/N): ");
-            choice = scanner.nextLine().toUpperCase().charAt(0);
-        } while (choice == 'Y');
+    public static double calculate(double num1, double num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    return Double.NaN; // Indicate error
+                }
+            default:
+                return Double.NaN; // Indicate error
+        }
     }
     
 }
