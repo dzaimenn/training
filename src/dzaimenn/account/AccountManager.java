feb457c9e9b4f01fb61b8ed4bca7e473086b490d
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-11-01 21:17:42 +0200

    Added the AccountManager class to manage accounts

---
+++ src/dzaimenn/account/AccountManager.java
@@ -0,0 +1,36 @@
+package dzaimenn.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountManager {
+    private List<Account> accounts;
+
+    public AccountManager() {
+        accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+        System.out.println("Account " + account.getAccountNumber() + " added successfully.");
+    }
+
+    public void removeAccount(String accountNumber) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNumber().equals(accountNumber)) {
+                accounts.remove(i);
+                System.out.println("Account " + accountNumber + " removed successfully.");
+                return;
+            }
+        }
+        System.out.println("Account " + accountNumber + " not found.");
+    }
+
+    public void displayAllAccounts() {
+        System.out.println("All accounts:");
+        for (Account account : accounts) {
+            System.out.println("Account Number: " + account.getAccountNumber() + ", Balance: " + account.getBalance());
+        }
+    }
+    
+}
